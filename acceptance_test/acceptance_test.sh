23f85f2c74baad7c821edb60d7b84dd2092fdf59
David Wickes <dave.wickes@gmail.com>
2018-08-31 11:13:48 +0100

    adding new item test

--- acceptance_test/acceptance_test.sh
+++ acceptance_test/acceptance_test.sh
@@ -1,25 +1,44 @@
 #!/bin/bash
 
-### GET test
-
 app_uri=$1
-page=$(curl -XGET $app_uri)
 
-fail_get=1
+### Get the page test
 
-get_content="h1"
-if grep -q "h1" <<< "$page"; then
-  fail_get=0
-fi
+get_test () {
+	page=$(curl -s -XGET $app_uri)
+	get_content="h1"
+
+	if ! grep -q "h1" <<< "$page"; then
+		pass_get=false
+		global_failures=true
+	fi
+
+	if [[ $pass_get = false ]]; then
+		printf "\tGET test... FAILED\n"
+		printf "Expected $page\n"
+		printf "to contain $get_content\n"
+	else
+		printf "\tGET test... PASSED\n"
+	fi
+}
+
+### New todo test
+new_todo_test () {
+	page=$(curl -L -s -F 'item=build todo app' $app_uri)
+	expected_content="build todo app"
+
+	if ! grep -q "$expected_content" <<< "$page"; then
+		global_failures=true
+		printf "\tNew todo test... FAILED\n"
+		printf "Expected $page\n"
+		printf "to contain $expected_content\n"
+	else
+		printf "\tNew todo test... PASSED\n"
+	fi
+}
 
 ### RESULTS
 
 printf "\nRESULTS\n"
-
-if [[ $fail_get = 1 ]]; then
-  printf "GET test... FAILED"
-	printf "Expected $page"
-	printf "to contain $get_content"
-else
-  printf "\tGET test... PASSED\n"
-fi
+get_test
+new_todo_test
