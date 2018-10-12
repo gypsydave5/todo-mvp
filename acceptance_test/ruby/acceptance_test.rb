3804857e94d8a0fc775beb0508ad6c1d905557a5
David Wickes <dave.wickes@gmail.com>
2018-10-12 14:37:05 +0100

    Ruby for acceptance test

--- acceptance_test/ruby/acceptance_test.rb
+++ acceptance_test/ruby/acceptance_test.rb
@@ -1,55 +1,45 @@
-#!/bin/env ruby
+#!/usr/bin/env ruby
 
 require 'faraday'
 require 'faraday_middleware'
-require 'nokogiri'
 require 'nokogumbo'
+require 'minitest'
 
-$http = Faraday.new("http://localhost:3000") do |conn|
-  conn.request  :url_encoded
-  conn.use FaradayMiddleware::FollowRedirects, limit: 5
-  conn.adapter Faraday.default_adapter
-end
+class TodoMVPAcceptance < Minitest::Test
+  i_suck_and_my_tests_are_order_dependent!
 
-$item_name = 'item_name'
+  def setup
+    @http = Faraday.new("http://localhost:3000") do |conn|
+      conn.request  :url_encoded
+      conn.use FaradayMiddleware::FollowRedirects, limit: 5
+      conn.adapter Faraday.default_adapter
+    end
 
-def test_the_starting_page_has_the_right_html
-  expected = Nokogiri.HTML5(File.read("golden_master.html")).to_s
-  actual = Nokogiri::HTML5($http.get('/').body).to_s
-  if expected != actual
-    puts "Starting page does not have the right HTML"
-    exit 1
-  end
-end
-
-def test_adding_a_todo
-  response = Nokogiri::HTML5($http.post('/', item: $item_name).body)
-  if response.css('.item-name').first.inner_html.strip != $item_name
-    puts "Did not add a todo item"
-    exit 1
-  end
-end
-
-def test_completing_a_todo
-  response = Nokogiri::HTML5($http.post('/done', item: $item_name).body)
-  if response.css('s').first.inner_html.strip != $item_name
-    puts "Did not add a todo item"
-    exit 1
+    @item_name = 'item_name'
   end
 
-end
+  def test_the_starting_page_has_the_right_html
+    expected = Nokogiri.HTML5(File.read("golden_master.html")).to_s
+    actual = Nokogiri::HTML5(@http.get('/').body).to_s
+    assert_equal expected, actual
+    if expected != actual
+      puts "Starting page does not have the right HTML"
+      exit 1
+    end
+  end
 
-def test_deleting_a_todo
-  response = Nokogiri::HTML5($http.post('/delete', item: $item_name).body)
-  if response.css('.item-name').length != 0
-    puts "Did not delete a todo item"
-    exit 1
+  def test_adding_a_todo
+    response = Nokogiri::HTML5(@http.post('/', item: @item_name).body)
+    assert_equal response.css('.item-name').first.inner_html.strip, @item_name
+  end
+
+  def test_completing_a_todo
+    response = Nokogiri::HTML5(@http.post('/done', item: @item_name).body)
+    assert_equal response.css('s').first.inner_html.strip,  @item_name
+  end
+
+  def test_deleting_a_todo
+    response = Nokogiri::HTML5(@http.post('/delete', item: @item_name).body)
+    assert_equal response.css('.item-name').length, 0
   end
 end
-
-test_the_starting_page_has_the_right_html
-test_adding_a_todo
-test_completing_a_todo
-test_deleting_a_todo
-puts "Acceptance tests pass"
-exit 0
