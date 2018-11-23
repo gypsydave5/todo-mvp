c64cd52108318e0beb5e74fe17bc097da9328c46
David Wickes <dave.wickes@gmail.com>
2018-11-23 12:06:58 +0000

    node - on right port, nicer error handling

--- todos/node.js/todo.js
+++ todos/node.js/todo.js
@@ -1,71 +1,81 @@
 const url = require('url')
 const http = require('http')
-const qs = require('querystring');
+const qs = require('querystring')
 const template = require('./template.html.js')
 
-const server = http.createServer()
+let todos = []
 
-var todos = []
-
-function add(item) {
-  todos.push({name: item, done: false})
+function add (item) {
+  todos.push({ name: item, done: false })
 }
 
-function toggle(item) {
+function toggle (item) {
   todos = todos.map(todo => todo.name === item
-                    ? { name: item, done: !todo.done }
-                    : todo)
+    ? { name: item, done: !todo.done }
+    : todo)
 }
 
-function remove(item) {
+function remove (item) {
   todos = todos.filter(todo => todo.name !== item)
 }
 
-server.on('request', (request, response) => {
-  switch(request.method) {
-  case 'GET':
-    return response.end(template(todos))
-  case 'POST':
-    parseForm(request).then(formdata => {
-      const requestUrl = url.parse(request.url, true)
-      const item = formdata.item
+const server = http.createServer()
 
-      switch(requestUrl.pathname) {
-      case "/done":
-        toggle(item)
-        break;
-      case "/not-done":
-        toggle(item)
-        break;
-      case "/delete":
-        remove(item)
-        break;
-      default:
-        add(item)
-        break;
-      }
-      redirectToHome(response)
-    })
+server.on('request', (request, response) => {
+  switch (request.method) {
+    case 'GET':
+      return response.end(template(todos))
+    case 'POST':
+      parseForm(request).then(formdata => {
+        const requestUrl = url.parse(request.url, true)
+        const item = formdata.item
+
+        switch (requestUrl.pathname) {
+          case '/done':
+            toggle(item)
+            break
+          case '/not-done':
+            toggle(item)
+            break
+          case '/delete':
+            remove(item)
+            break
+          default:
+            add(item)
+            break
+        }
+        redirectToHome(response)
+      })
   }
 })
 
-function parseForm(request) {
-  return new Promise((fulfill, reject) => {
+function parseForm (request) {
+  return new Promise((resolve, reject) => {
     const chunks = []
 
     request.on('data', chunk => chunks.push(chunk))
 
     request.on('end', () => {
       const body = Buffer.concat(chunks).toString()
-      fulfill(qs.parse(body))
+      resolve(qs.parse(body))
     })
   })
 }
 
-function redirectToHome(response) {
+function redirectToHome (response) {
   response.statusCode = 303
   response.setHeader('Location', '/')
   response.end()
 }
 
-server.listen(4000)
+const port = 3000
+server.on('error', e => {
+  console.error(`Server error: ${e}\nShutting down`)
+  process.exit(1)
+})
+
+server.on('listening', e => {
+  console.log(`Listening on port ${port}`)
+})
+
+server.listen(port)
