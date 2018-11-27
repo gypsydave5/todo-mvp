b8c530878aff37065839e86255b53821236c4fc5
Nathan Graule <solarliner@gmail.com>
2018-11-27 20:40:29 +0100

    Add todos array in lieu of proper state management

--- todos/flask/todomvp/routes.py
+++ todos/flask/todomvp/routes.py
@@ -1,6 +1,15 @@
 from flask import render_template
 from todomvp import app
 
+todos = [
+    {
+        "id": 1,
+        "name": "Make TODOs Great Again",
+        "done": False
+    }
+]
+
+
 @app.route("/")
 def index():
-    return render_template("home.html")
+    return render_template("home.html", todos=todos)
