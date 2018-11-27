acbffc5356def32fd0eab92dae09bb019726f72a
Nathan Graule <solarliner@gmail.com>
2018-11-27 22:09:57 +0100

    Add note about manual scaffolding

--- todos/django/todomvp/__init__.py
+++ todos/django/todomvp/__init__.py
@@ -1,3 +1,7 @@
+# Note: I am manually creating a tiny project here since Django is much more
+# powerful and a bit too much of a powerhouse for this tiny project ;)
+# Django's new project command adds a whole lot more stuff that make sense for a
+# typical server, but not here.
 DEBUG = True
 SECRET_KEY = '4l0ngs3cr3tstr1ngw3lln0ts0l0ngw41tn0w1tsl0ng3n0ugh'
 ROOT_URLCONF = __name__
