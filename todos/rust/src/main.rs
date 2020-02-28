26f21943e4b3da1fc60c1cd98afa856fade47791
Ben Lovy <ben@deciduously.com>
2020-02-28 06:48:17 -0500

    Migrate Rust app to async/await syntax

--- todos/rust/src/main.rs
+++ todos/rust/src/main.rs
@@ -1,15 +1,10 @@
-#[macro_use]
-extern crate lazy_static;
-#[macro_use]
-extern crate serde_derive;
-#[macro_use]
-extern crate tera;
-
-use futures::{future, Future, Stream};
 use hyper::{
-    client::HttpConnector, header, rt, service::service_fn, Body, Client, Method, Request,
-    Response, Server, StatusCode,
+    header,
+    service::{make_service_fn, service_fn},
+    Body, Method, Request, Response, Server, StatusCode,
 };
+use lazy_static::lazy_static;
+use serde::Serialize;
 use std::{
     path::PathBuf,
     sync::{Arc, RwLock},
@@ -17,9 +12,7 @@ use std::{
 use tera::{Context, Tera};
 use uuid::Uuid;
 
-// Handler type aliases
-type GenericError = Box<dyn std::error::Error + Send + Sync>;
-type ResponseFuture = Box<Future<Item = Response<Body>, Error = GenericError> + Send>;
+type HandlerResult = Result<Response<Body>, anyhow::Error>;
 
 // Stand-in 404 response
 static NOTFOUND: &[u8] = b"Oops! Not Found";
@@ -48,7 +41,13 @@ type Todos = Arc<RwLock<Vec<Todo>>>;
 // Initialize Tera template and todo state
 // This app only has one file in one directory, but this way it's ready for you to organize
 lazy_static! {
-    pub static ref TERA: Tera = compile_templates!("templates/**/*");
+    pub static ref TERA: Tera = match Tera::new("templates/**/*") {
+        Ok(t) => t,
+        Err(e) => {
+            eprintln!("Parsing error(s): {}", e);
+            std::process::exit(1);
+        }
+    };
     pub static ref TODOS: Todos = Arc::new(RwLock::new(Vec::new()));
 }
 
@@ -88,17 +87,13 @@ fn toggle_todo(id: Uuid) {
 
 // Routes
 
-fn four_oh_four() -> ResponseFuture {
-    let body = Body::from(NOTFOUND);
-    Box::new(future::ok(
-        Response::builder()
-            .status(StatusCode::NOT_FOUND)
-            .body(body)
-            .unwrap(),
-    ))
+async fn four_oh_four() -> HandlerResult {
+    Ok(Response::builder()
+        .status(StatusCode::NOT_FOUND)
+        .body(Body::from(NOTFOUND))?)
 }
 
-fn index() -> ResponseFuture {
+async fn index() -> HandlerResult {
     // Set up index page template rendering context
     let mut ctx = Context::new();
     let todos = Arc::clone(&TODOS);
@@ -106,22 +101,19 @@ fn index() -> ResponseFuture {
     ctx.insert("todos", &*lock);
     ctx.insert("todosLen", &(*lock).len());
     // Render the index template with the context
-    let body = Body::from(TERA.render("index.html", &ctx).unwrap().to_string());
-    Box::new(future::ok(Response::new(body)))
+    let body = Body::from(TERA.render("index.html", &ctx)?.to_string());
+    Ok(Response::new(body))
 }
 
-fn stylesheet() -> ResponseFuture {
+async fn stylesheet() -> HandlerResult {
     let body = Body::from(include_str!("resource/todo.css"));
-    Box::new(future::ok(
-        Response::builder()
-            .status(StatusCode::OK)
-            .header(header::CONTENT_TYPE, "text/css")
-            .body(body)
-            .unwrap(),
-    ))
+    Ok(Response::builder()
+        .status(StatusCode::OK)
+        .header(header::CONTENT_TYPE, "text/css")
+        .body(body)?)
 }
 
-fn image(path_str: &str) -> ResponseFuture {
+async fn image(path_str: &str) -> HandlerResult {
     let path_buf = PathBuf::from(path_str);
     let file_name = path_buf.file_name().unwrap().to_str().unwrap();
     let ext = path_buf.extension().unwrap().to_str().unwrap();
@@ -140,115 +132,80 @@ fn image(path_str: &str) -> ResponseFuture {
                 };
                 Body::from(xml)
             };
-            Box::new(future::ok(
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header(header::CONTENT_TYPE, "image/svg+xml")
-                    .body(body)
-                    .unwrap(),
-            ))
+            Ok(Response::builder()
+                .status(StatusCode::OK)
+                .header(header::CONTENT_TYPE, "image/svg+xml")
+                .body(body)?)
         }
-        _ => four_oh_four(),
+        _ => four_oh_four().await,
     }
 }
 
-fn redirect_home() -> ResponseFuture {
-    Box::new(future::ok(
-        Response::builder()
-            .status(StatusCode::SEE_OTHER)
-            .header(header::LOCATION, "/")
-            .body(Body::from(""))
-            .unwrap(),
-    ))
+async fn redirect_home() -> HandlerResult {
+    Ok(Response::builder()
+        .status(StatusCode::SEE_OTHER)
+        .header(header::LOCATION, "/")
+        .body(Body::from(""))?)
 }
 
-fn add_todo_handler(req: Request<Body>) -> ResponseFuture {
-    Box::new(
-        req.into_body()
-            .concat2() // concatenate all the chunks in the body
-            .from_err() // like try! for Result, but for Futures
-            .and_then(|whole_body| {
-                let str_body = String::from_utf8(whole_body.to_vec()).unwrap();
-                let words: Vec<&str> = str_body.split('=').collect();
-                add_todo(Todo::new(words[1]));
-                redirect_home()
-            }),
-    )
+async fn add_todo_handler(req: Request<Body>) -> HandlerResult {
+    let body = hyper::body::to_bytes(req).await?;
+    let str_body = String::from_utf8(body.to_vec()).unwrap();
+    let words: Vec<&str> = str_body.split('=').collect();
+    add_todo(Todo::new(words[1]));
+    redirect_home().await
 }
 
-fn remove_todo_handler(req: Request<Body>) -> ResponseFuture {
-    Box::new(
-        req.into_body()
-            .concat2() // concatenate all the chunks in the body
-            .from_err() // like try! for Result, but for Futures
-            .and_then(|whole_body| {
-                let str_body = String::from_utf8(whole_body.to_vec()).unwrap();
-                let words: Vec<&str> = str_body.split('=').collect();
-                remove_todo(Uuid::parse_str(words[1]).unwrap());
-                redirect_home()
-            }),
-    )
+async fn remove_todo_handler(req: Request<Body>) -> HandlerResult {
+    let body = hyper::body::to_bytes(req).await?;
+    let str_body = String::from_utf8(body.to_vec()).unwrap();
+    let words: Vec<&str> = str_body.split('=').collect();
+    remove_todo(Uuid::parse_str(words[1]).unwrap());
+    redirect_home().await
 }
 
-fn toggle_todo_handler(req: Request<Body>) -> ResponseFuture {
-    Box::new(
-        req.into_body()
-            .concat2() // concatenate all the chunks in the body
-            .from_err() // like try! for Result, but for Futures
-            .and_then(|whole_body| {
-                let str_body = String::from_utf8(whole_body.to_vec()).unwrap();
-                let words: Vec<&str> = str_body.split('=').collect();
-                toggle_todo(Uuid::parse_str(words[1]).unwrap());
-                redirect_home()
-            }),
-    )
+async fn toggle_todo_handler(req: Request<Body>) -> HandlerResult {
+    let body = hyper::body::to_bytes(req).await?;
+    let str_body = String::from_utf8(body.to_vec()).unwrap();
+    let words: Vec<&str> = str_body.split('=').collect();
+    toggle_todo(Uuid::parse_str(words[1]).unwrap());
+    redirect_home().await
 }
 
-fn router(req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFuture {
+async fn router(req: Request<Body>) -> HandlerResult {
     // pattern match for both the method and the path of the request
     match (req.method(), req.uri().path()) {
         // GET handlers
         // Index page handler
-        (&Method::GET, "/") | (&Method::GET, "/index.html") => index(),
+        (&Method::GET, "/") | (&Method::GET, "/index.html") => index().await,
         // Style handler
-        (&Method::GET, "/static/todo.css") => stylesheet(),
+        (&Method::GET, "/static/todo.css") => stylesheet().await,
         // Image handler
-        (&Method::GET, path_str) => image(path_str),
+        (&Method::GET, path_str) => image(path_str).await,
         // POST handlers
-        (&Method::POST, "/done") => toggle_todo_handler(req),
-        (&Method::POST, "/not-done") => toggle_todo_handler(req),
-        (&Method::POST, "/delete") => remove_todo_handler(req),
-        (&Method::POST, "/") => add_todo_handler(req),
+        (&Method::POST, "/done") => toggle_todo_handler(req).await,
+        (&Method::POST, "/not-done") => toggle_todo_handler(req).await,
+        (&Method::POST, "/delete") => remove_todo_handler(req).await,
+        (&Method::POST, "/") => add_todo_handler(req).await,
         // Anything else handler
-        _ => four_oh_four(),
+        _ => four_oh_four().await,
     }
 }
 
 // Entrypoint
 
-fn main() {
-    pretty_env_logger::init();
-
+#[tokio::main]
+async fn main() {
     // .parse() parses to a std::net::SocketAddr
     let addr = "127.0.0.1:3000".parse().unwrap();
+    let make_svc =
+        make_service_fn(|_conn| async { Ok::<_, anyhow::Error>(service_fn(router)) });
 
-    rt::run(future::lazy(move || {
-        // create a Client for all Services
-        let client = Client::new();
+    let server = Server::bind(&addr).serve(make_svc);
 
-        // define a service containing the router function
-        let new_service = move || {
-            // Move a clone of Client into the service_fn
-            let client = client.clone();
-            service_fn(move |req| router(req, &client))
-        };
+    println!("Serving {} on {}", env!("CARGO_PKG_NAME"), addr);
 
-        // Define the server - this is what the future_lazy() we're building will resolve to
-        let server = Server::bind(&addr)
-            .serve(new_service)
-            .map_err(|e| eprintln!("Server error: {}", e));
-
-        println!("Listening on http://{}", addr);
-        server
-    }));
+    if let Err(e) = server.await {
+        eprintln!("Server error: {}", e);
+    }
 }
